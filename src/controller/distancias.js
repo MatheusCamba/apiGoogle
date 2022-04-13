d3df62cd9ac6fdbec4a789f8f2dc27758ee68ce8
Matheus Camba <mhscamba@gmail.com>
2022-04-13 19:20:51 -0300

    distancias add

--- src/controller/distancias.js
+++ src/controller/distancias.js
@@ -1,5 +1,22 @@
+import GoogleService from "../services/googleService.js";
+import Calculo from "../utils/calculo.js";
+
 class Distancias {
-  static router(app) {}
+  static router(app) {
+    app.get("/", async (req, res) => {
+      const queries = req.query;
+      const queriesValues = Object.values(queries);
+
+      GoogleService.groupedCoordinate(...queriesValues)
+        .then((response) => {
+          const result = Calculo.distanceMaxMin(response);
+          res.status(200).json(result);
+        })
+        .catch((e) => {
+          res.status(400).json(e.message);
+        });
+    });
+  }
 }
 
 export default Distancias;
