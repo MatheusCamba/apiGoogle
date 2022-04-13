16f9df197b3a28c899d834b3159ed2e55a4eb28a
Matheus Camba <mhscamba@gmail.com>
2022-04-13 19:20:10 -0300

    app add

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 import express from "express";
-import Distancias from "./src/controller/distancias";
+import Distancias from "./src/controller/distancias.js";
+import * as dotenv from "dotenv";
+import cors from "cors";
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 
 app.listen(port, () => {
   console.log(`Servidor em http://localhost:3000`);
