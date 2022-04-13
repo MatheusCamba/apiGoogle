b36f199a8170fb6d3f44a7b1e27eb82824a40515
Matheus Camba <mhscamba@gmail.com>
2022-04-13 19:21:56 -0300

    googleService add

--- src/services/googleService.js
+++ src/services/googleService.js
@@ -1,7 +1,14 @@
 import axios from "axios";
-import LocationModel from "../model/LocationModel";
+import { LocationModel } from "../model/LocationModel.js";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 class GoogleService {
+  /**
+   *
+   * @param {string} address
+   * @returns Object
+   */
   static async geolocation(address) {
     const endCode = encodeURIComponent(address);
     const key = process.env.API_KEY;
@@ -10,15 +17,21 @@ class GoogleService {
         `https://maps.googleapis.com/maps/api/geocode/json?address=${endCode}&key=${key}`
       )
     ).data;
-    return new LocationModel(request[0].geometry.location, address)
-  }
 
-  static groupedCordinate(...params){
-      const grouped = []
-      for(let i = 0; i < params[0]; i++){
-        let address = await this.geolocation(params[0][i])
-        grouped.push(address)
-      }
+    return new LocationModel(request.results[0].geometry.location, address);
+  }
+  /**
+   *
+   * @param  {...any} params
+   * @returns {Array<Object>}
+   */
+  static async groupedCoordinate(...params) {
+    const grouped = [];
+    for (let i = 0; i < params.length; i++) {
+      let address = await this.geolocation(params[i]);
+      grouped.push(address);
+    }
+    return grouped;
   }
 }
 
