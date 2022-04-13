c9c9b1496b58a1464def6ee4fc128cbb837104dc
Matheus Camba <mhscamba@gmail.com>
2022-04-13 19:21:36 -0300

    locationmodel add

--- src/model/LocationModel.js
+++ src/model/LocationModel.js
@@ -1,8 +1,27 @@
-class LocationModel {
-  constructor(cordinate, adress) {
-    this.cordinate = cordinate;
-    this.adress = adress;
+export class LocationModel {
+  constructor(coordinate, address) {
+    this.coordinate = coordinate;
+    this.address = address;
   }
 }
 
-export default LocationModel;
+export class Path {
+  constructor(startingPoint, destiny) {
+    this.startingPoint = startingPoint;
+    this.destiny = destiny;
+  }
+}
+
+export class FormattedDistance {
+  constructor(path, distance) {
+    this.path = path;
+    this.distance = distance;
+  }
+}
+
+export class Response {
+  constructor(closer, futher) {
+    this.close = closer;
+    this.futher = futher;
+  }
+}
